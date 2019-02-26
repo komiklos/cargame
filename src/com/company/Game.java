b1b0f71eb9363823aaf95d3532056814cb3f7c8e
garyAdam <rapture047@gmail.com>
2019-02-26 22:18:05 +0100

    Game now adjusts to terminal size

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -2,40 +2,71 @@ package com.company;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 import com.codecool.termlib.*;
+import java.awt.*;
 
 public class Game{
+  private int[] terminalSize = new int[2];
+  private int terminalHeight;
+  private int terminalWidth;
   private Terminal t = new Terminal();
   private Queue<Character> road = new LinkedList<>();
   public void init(){
-
-
     t.clearScreen();
-    t.moveTo(31,1);
+    this.terminalSize = getTerminalSize();
+    this.terminalHeight = this.terminalSize[0];
+    this.terminalWidth = this.terminalSize[1];
+    t.moveTo(terminalHeight,1);
     System.out.print(generateLowerRoad());
     generateRoad();
     printUpperRoad();
     printCar(0);
+    System.out.print(terminalWidth);
+
     while (true) {
 
     }
 
   }
+  private int[] getTerminalSize(){
+    t.moveTo(3100,1000);
+    int[] size = new int[2];
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("\033[6n");
+    try
+    {
+      Robot enterpresser = new Robot();
+      enterpresser.keyPress(10);
+      enterpresser.keyRelease(10);
+    }
+    catch (AWTException e)
+    {
+      e.printStackTrace();
+    }
+    String response = scanner.next();
+    t.clearScreen();
+    scanner.close();
+    String[] splitresponse = response.split(";");
+    size[0] = Integer.parseInt( splitresponse[0].substring(2, splitresponse[0].length()));
+    size[1] = Integer.parseInt( splitresponse[1].substring(0, splitresponse[1].length()-1));
+
+    return size;
+  }
   private String generateLowerRoad(){
     String lowerRoad = "";
-    for(int i = 0; i< 122; i++){
+    for(int i = 0; i< this.terminalWidth; i++){
       lowerRoad+= '#';
     }
     return lowerRoad;
   }
   private void generateRoad(){
-    for(int i = 0; i< 122; i++){
+    for(int i = 0; i< this.terminalWidth; i++){
       this.road.add('#');
     }
 
 
   }
   private void printUpperRoad(){
-    t.moveTo(30,1);
+    t.moveTo(this.terminalHeight-1,1);
     for(char ch : this.road){
       System.out.print(ch);
     }
@@ -47,7 +78,7 @@ public class Game{
     car[2] = " (o _ |  -|     o|";
     car[3] = "  `(_)-------(_)-'";
     for(int i = 0; i < 4; i++){
-      t.moveTo(26+i,60);
+      t.moveTo(this.terminalHeight-5+i,this.terminalWidth/2);
       System.out.print(car[i]);
     }
 
